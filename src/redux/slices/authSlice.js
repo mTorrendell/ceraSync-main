86fa10c779ba02b3bebef5f52dd4cfa8ebbedfdd
node-value <gumangi3579@gmail.com>
2023-12-05 21:16:23 +0100

    Install redux, authSclice implemented

---
+++ src/redux/slices/authSlice.js
@@ -0,0 +1,98 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const baseURL = axios.create({ baseURL: "http://cerasync-back-49c53729469a.herokuapp.com/api/auth" });
+
+const initialState = {
+    isLogged:      false,
+    isEmailExists: undefined,
+    currEmail:     "",
+    isError:       false,
+    errorMsg:      ""
+};
+
+export const authenticate = createAsyncThunk("auth_slice/authenticate", async (user) => {
+    const response = await baseURL.post("/authenticate", user);
+    return response.data;
+});
+
+export const register = createAsyncThunk("auth_slice/register", async (user) => {
+    const response = await baseURL.post("/register", user);
+    return response.data;
+});
+
+export const checkEmail = createAsyncThunk("auth_slice/checkEmail", async (user) => {
+    const response = await baseURL.post("/email_check", user);
+    return response.data; 
+});
+
+const authSlice = createSlice({
+    name: "auth_slice",
+    initialState,
+    reducers: {
+        setIsLogged(state, action) { 
+            state.isLogged = action.payload; 
+        }, 
+        setIsEmailExists(state, action) {
+            state.isEmailExists = action.payload;
+        },
+        setCurrEmail(state, action) {
+            state.currEmail = action.payload
+        },
+        setErrorAuth(state, action) {
+            state.isError  = action.payload.isError;
+            state.errorMsg = action.payload.errorMsg;
+        }
+    }, 
+    extraReducers(builder) {
+        builder
+            .addCase(authenticate.fulfilled, (state, action) => {
+                state.isLogged = true;
+                localStorage.setItem("CERASYNC_JWT_TOKEN", action.payload.token);
+            })
+            .addCase(authenticate.rejected, (state, action) => {
+                console.log(action.error);
+                state.isError = true;
+                state.errorMsg = action.error.code === 'ERR_BAD_REQUEST' ? 
+                    "Wrong username or password" : 
+                    `Failed to authenticate: ${action.error.message}`;   
+            })
+            .addCase(register.fulfilled, (state, action) => {
+                state.isLogged = true;
+                localStorage.setItem("CERASYNC_JWT_TOKEN", action.payload.token);
+            })
+            .addCase(register.rejected, (state, action) => {
+                console.log(action.error);
+                state.isError  = true;
+                state.errorMsg = action.error.code === 'ERR_BAD_REQUEST' ? 
+                // Test this: action.error.response.status === 400
+                    "Username has aleready taken" : 
+                    `Failed to register: ${action.error.message}`;
+            })
+            .addCase(checkEmail.fulfilled, (state, action) => {
+                state.isEmailExists = action.payload.isExists;
+                state.currEmail     = action.payload.email;
+            })
+            .addCase(checkEmail.rejected, (state, action) => {
+                console.log(action.error);
+                state.isError = true;
+                state.errorMsg = "Failed to check the email."
+            });
+
+    }
+});
+
+export const { 
+    setIsLogged, 
+    setIsEmailExists, 
+    setCurrEmail, 
+    setErrorAuth 
+} = authSlice.actions;
+
+export const selectIsLogged      = (state) => state.auth.isLogged;
+export const selectIsEmailExists = (state) => state.auth.isEmailExists;
+export const selectCurrEmail     = (state) => state.auth.currEmail;
+export const selectIsError       = (state) => state.auth.isError;
+export const selectErrorMsh      = (state) => state.auth.errorMsg;
+
+export default authSlice.reducer;
