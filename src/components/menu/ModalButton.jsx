38403c89702989f6fd4a9c46c132f1c1770c25e5
node-value <gumangi3579@gmail.com>
2023-12-07 20:04:23 +0100

    Add word menu to MenuButton

--- src/components/menu/ModalButton.jsx
+++ src/components/menu/ModalButton.jsx
@@ -3,13 +3,13 @@ import "./styles/ModalButton.css";
 import logo from "../img/Logo.svg";
 
 const ModalButton = ({ openModal }) => {
-
     return (
         <div id="logo_container" >
             <canvas id="canvas" onClick={openModal} ></canvas>
             <object id="logoimg" type="image/svg+xml" data={logo}>
                 Your browser does not support svg
             </object>
+            <div id="menu_botton_text">Menu</div>
         </div>
     )
 }
